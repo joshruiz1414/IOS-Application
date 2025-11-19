475cc8dc984549ab8b022af8eabe9508e4b2015c
Joshua Ruiz <joshrn2002@gmail.com>
2025-11-18 21:04:33 -0800

    ENH: Inital commit creating project

---
+++ Booksie/Booksie/BooksieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksieApp.swift
+//  Booksie
+//
+//  Created by Joshua Ruiz on 2025-11-14.
+//
+
+import SwiftUI
+
+@main
+struct BooksieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Booksie/BooksieTests/BooksieTests.swift
@@ -0,0 +1,17 @@
+//
+//  BooksieTests.swift
+//  BooksieTests
+//
+//  Created by Joshua Ruiz on 2025-11-14.
+//
+
+import Testing
+@testable import Booksie
+
+struct BooksieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
